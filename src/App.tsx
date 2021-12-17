778c5013eb492a4bd6527bea6427d56ef0469ee2
juliawang2014 <59949033+juliawang2014@users.noreply.github.com>
2021-12-17 12:10:03 -0500

    Init Firebase SDK

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,26 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyA_YpeR-nCdjPFCbQREO3Bg9Dp4BIWUPpk",
+  authDomain: "oututoringdatabase.firebaseapp.com",
+  projectId: "oututoringdatabase",
+  storageBucket: "oututoringdatabase.appspot.com",
+  messagingSenderId: "763676444526",
+  appId: "1:763676444526:web:b0eac5aa6892e9a2ac237f",
+  measurementId: "G-CC2H30MC7N"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
 
 function App() {
   return (
