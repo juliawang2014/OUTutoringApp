8f5892ce33aefe4a1e5a36b2868723a788d4ab43
tarek-sanat <tarektanas20@gmail.com>
2021-12-30 02:38:49 -0500

    Fetch from DB and display user info in the page
    Firebase DB API used was changed. Needs to be reset to original one after modifications.
    Makes calls to firebase database to fetch users and display them in the UI.
    
    This version uses states to rerender the  UI when changes happen.

--- src/App.tsx
+++ src/App.tsx
@@ -1,45 +1,90 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useEffect, useState} from 'react';
 import './App.css';
 import { initializeApp } from "firebase/app";
-import { getAnalytics, logEvent } from "firebase/analytics";
+import { getFirestore, collection, getDocs, doc, getDoc } from "firebase/firestore";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyA_YpeR-nCdjPFCbQREO3Bg9Dp4BIWUPpk",
-  authDomain: "oututoringdatabase.firebaseapp.com",
-  projectId: "oututoringdatabase",
-  storageBucket: "oututoringdatabase.appspot.com",
-  messagingSenderId: "763676444526",
-  appId: "1:763676444526:web:b0eac5aa6892e9a2ac237f",
-  measurementId: "G-CC2H30MC7N"
+  apiKey: "AIzaSyAP4yOpNgGDrzhDI9lKSFhA3ag8Zm1R1gM",
+  authDomain: "oucalendar-2c0b8.firebaseapp.com",
+  databaseURL: "https://oucalendar-2c0b8-default-rtdb.firebaseio.com",
+  projectId: "oucalendar-2c0b8",
+  storageBucket: "oucalendar-2c0b8.appspot.com",
+  messagingSenderId: "228349542652",
+  appId: "1:228349542652:web:ae74f29e9710bb67905779",
+  measurementId: "G-5EPCP4ZBBM"
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
-logEvent(analytics, 'notification_received');
+const db = getFirestore();
+
+
+
 
 function App() {
+  //All users list state
+  let [users, setUsersState] = useState([])
+  //Single User state
+  let [user, setUserState] = useState()
+  
+
+  //Fetch single User from database
+  const fetchUsers = async () => {
+    let userList:any = []
+    //query all the documents from the users collection from database
+    const querySnapshot = await getDocs(collection(db, "users"));
+    //loop through those documents and concatenate each user to usersList
+    querySnapshot.forEach((doc) => {
+      userList.push(
+        <div key ={doc.id}>{'Username ' + doc.data().name+ ' age is ' + doc.data().age + ' id is ' + doc.id}</div>
+        )
+            
+    });
+    //set new state as the concatenated list 
+    // The reason why state is used is because everytime it is updated, the component UI will be rerendered containing the fetched data
+    setUsersState(userList)
+    
+  }
+  
+
+  // Fetch single user from database
+  const fetchUser = async() => {
+    let userList:any = []
+    //https://firebase.google.com/docs/firestore/query-data/get-data From Get A document
+    // get single document from users where the id is given
+    const docSnap = await getDoc(doc(db, "users", "iPWWIldQZVbtwZVNsCyY"));
+    console.log("Document data:", docSnap.data());
+    // if a user is returned add the user to a list
+    if(docSnap.exists()){
+      userList.push(
+        <div key ='random'>{'Username ' + docSnap.data().name+ ' age is ' + docSnap.data().age}</div>
+      )
+    }
+    // change user state to rerender the UI and have the user info displayed
+    setUserState(userList)
+    
+  }
+  
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      donut sucre au sucre
+      <div>
+        <p>This will fetch all users from the db</p>
+        {users}
+        <button onClick={fetchUsers}>Fetch Users</button>
+      </div>
+      <hr/>
+      <div>
+        <p>This will fetch one single user from the db with a specific ID</p>
+        {user}
+        <button onClick={fetchUser}>Fetch Users</button>
+      </div>
+      
     </div>
   );
 }
