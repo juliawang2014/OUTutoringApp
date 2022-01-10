8c9edefebe5de617c8d83eae4cd30f221bb7f6c3
Edgar Terrazas <ragdeterra@gmail.com>
2022-01-10 01:22:07 -0500

    Fix to make test pass

--- src/App.tsx
+++ src/App.tsx
@@ -3,19 +3,21 @@ import Home from './frontend/components/Home';
 import Me from './frontend/components/Me/Me';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import NavBar from './frontend/components/NavBar';
-import { Routes, Route } from 'react-router';
+import { Routes, Route, BrowserRouter as Router } from 'react-router-dom';
 import { FirebaseAuthProvider } from "./frontend/contexts/FirebaseAuthContext";
 
 function App() {
   return (
     <div className="App">
-      <FirebaseAuthProvider>
-        <NavBar />
-        <Routes>
-            <Route path="/" element={<Home/>}/>
-            <Route path="/me" element={<Me/>}/>
-        </Routes>
-      </FirebaseAuthProvider>
+      <Router>
+        <FirebaseAuthProvider>
+          <NavBar />
+          <Routes>
+              <Route path="/" element={<Home/>}/>
+              <Route path="/me" element={<Me/>}/>
+          </Routes>
+        </FirebaseAuthProvider>
+      </Router>
     </div>
   );
 }
