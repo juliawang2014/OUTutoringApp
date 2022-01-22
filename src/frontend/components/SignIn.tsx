534c2bec299925d91acefc341b6d0edcfb7e8c81
Tarek Sanat <tarektanas20@gmail.com>
2022-01-21 19:26:04 -0500

    Add new users to database on login
    When new users login, add them to database with their own id and an initial role of student.
    Also implements basic cloud function.

--- src/frontend/components/SignIn.tsx
+++ src/frontend/components/SignIn.tsx
@@ -1,12 +1,29 @@
 import { GoogleAuthProvider, signInWithPopup } from 'firebase/auth';
 import { auth } from '../services/firebase';
 import { Button } from 'react-bootstrap';
+import { doc, getDoc, setDoc } from "firebase/firestore";
+import {db} from '../services/firebase';
+
 
 const GoogleSignIn = () => {
   const signInWithGoogle = async () => {
     const googleProvider = new GoogleAuthProvider();
+    
     //googleProvider.setCustomParameters({ prompt: 'select_account' });
     await signInWithPopup(auth, googleProvider);
+    let user = await signInWithPopup(auth, googleProvider)
+    let docSnap = await getDoc(doc(db, "users", user.user.uid))
+    if(!docSnap.exists()){
+      console.log(user.user)
+      let data = {
+        name: user.user.displayName,
+        email: user.user.email,
+        role: 'Student',
+      }
+      await setDoc(doc(db, "users", user.user.uid), data)
+    } else{
+      console.log('user was found')
+    }
   }
 
   return (
